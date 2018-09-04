9b675330c8a8aa1f3f60de20de3c18a4eba6fb7f
Roma <roman.sheverun@jointoit.com>
2018-09-04 22:06:52 +0300

    fixed seeds and migrations

--- database/seeds/UsersSeed.php
+++ database/seeds/UsersSeed.php
@@ -16,6 +16,7 @@ class UsersSeed extends Seeder
             'email' => 'user1@user.com',
             'name' => 'UserOne',
             'password' => bcrypt('123qwe'),
+            'verified' => 1,
             'group_id' => 1,
          ]);
  
@@ -25,6 +26,7 @@ class UsersSeed extends Seeder
             'email' => 'user2@user.com',
             'name' => 'UserTwo',
             'password' => bcrypt('123qwe'),
+            'verified' => 1,
             'group_id' => 2,
          ]);
  
