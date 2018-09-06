be889c2274d5470154caa0c665f8f82649e1b743
Roma <roman.sheverun@jointoit.com>
2018-09-06 17:51:37 +0300

    add font Acrom

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -17,61 +17,23 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark  nav_menu">
-<div class="logo">
-  	<a  href="{{route('home')}}">My Hunting Area
-      <img src="{{asset('img/target.png')}}" alt="">  
-    </a>
-</div>
-  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbar1" aria-expanded="false" aria-label="Toggle navigation">
-    	<span class="navbar-toggler-icon"></span>
-  	</button>
-
-  	<div class="collapse navbar-collapse" id="navbar1">
-    	<ul class="navbar-nav mr-auto">
-      		<li class="nav-item">
-        		<a class="nav-link header-link" href="{{ route('home') }}">Home</a>
-      		</li>
-		    <li class="nav-item">
-		    	<a class="nav-link header-link" href="{{ route('cameras') }}">Cameras</a>
-            </li>
-            <li class="nav-item">
-		    	<a class="nav-link header-link" href="#">Images</a>
-		    </li>
-      		
-    	</ul>
-        <ul class="navbar-nav ml-auto">
-        <li class="nav-item dropdown user-item">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                <img src="{{asset('img/hunter.png')}}" class="img-fluid user-logo"></img>
-                {{ Auth::user()->name }} <span class="caret"></span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right custom-dropdown" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
-
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </li>
-        </ul>
-  	</div>
-</nav>    
+@include('layouts.nav')
     <div class="custom-container">
         <div class="content">
             @yield('content')        
         </div>
 
-    </div>        
+    </div>
+    
     <footer>
     <nav class="nav flex-column footer-nav">
+    @guest
+    
+    @else
         <a class="nav-link" href="{{ route('home') }}">home</a>
         <a class="nav-link" href="{{ route('cameras') }}">cameras</a>
         <a class="nav-link" href="#">images</a>
+    @endguest
     </nav>
         <div class="col-12 text-center">
         © 2018 My Hunting Area, Inc
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,56 @@
+<nav class="navbar navbar-expand-lg navbar-dark  nav_menu">
+<div class="logo">
+  	<a  href="{{route('home')}}">My Hunting Area
+      <img src="{{asset('img/target.png')}}" alt="">  
+    </a>
+</div>
+  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbar1" aria-expanded="false" aria-label="Toggle navigation">
+    	<span class="navbar-toggler-icon"></span>
+  	</button>
+    @guest
+    <div class="collapse navbar-collapse" id="navbar1">
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+        </ul>
+    </div>
+    @else
+  	<div class="collapse navbar-collapse" id="navbar1">
+    	<ul class="navbar-nav mr-auto">
+      		<li class="nav-item">
+        		<a class="nav-link header-link" href="{{ route('home') }}">Home</a>
+      		</li>
+		    <li class="nav-item">
+		    	<a class="nav-link header-link" href="{{ route('cameras') }}">Cameras</a>
+            </li>
+            <li class="nav-item">
+		    	<a class="nav-link header-link" href="#">Images</a>
+		    </li>
+      		
+    	</ul>
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown user-item">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                <img src="{{asset('img/hunter.png')}}" class="img-fluid user-logo"></img>
+                {{ Auth::user()->name }} <span class="caret"></span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right custom-dropdown" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </li>
+        </ul>
+      </div>
+      @endguest
+</nav>    
