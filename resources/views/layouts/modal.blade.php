aaeb6947457142243c2b01a3f0313bb8577a23e3
Roma <roman.sheverun@jointoit.com>
2018-10-10 20:10:20 +0300

    modal text

--- resources/views/layouts/modal.blade.php
+++ resources/views/layouts/modal.blade.php
@@ -17,3 +17,23 @@
     </div>
   </div>
 </div>
+<script>
+
+   function modal_data(id, title) { 
+    var a = document.getElementById("delete-btn");
+    a.setAttribute("value", id); 
+    
+    if(title == 'user_destroy') {
+        document.getElementById("title").innerHTML = "Delete user";
+        document.getElementById("text").innerHTML = "Are you sure you want to delete user?";
+    }
+    if(title == 'camera_destroy') {
+        document.getElementById("title").innerHTML = "Delete camera";
+        document.getElementById("text").innerHTML = "Are you sure you want to delete camera?";
+    }
+    if(title == 'image_destroy') {
+        document.getElementById("title").innerHTML = "Delete image";
+        document.getElementById("text").innerHTML = "Are you sure you want to delete image?";
+    }
+}
+</script>
